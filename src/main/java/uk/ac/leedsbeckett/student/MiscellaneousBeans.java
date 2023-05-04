6544271a94b865291d64c8228c87fa3a74185385
Sds0702 <seq.sam@gmail.com>
2023-05-04 15:16:04 +0100

    3rd dev commit

--- src/main/java/uk/ac/leedsbeckett/student/MiscellaneousBeans.java
+++ src/main/java/uk/ac/leedsbeckett/student/MiscellaneousBeans.java
@@ -12,11 +12,37 @@ public class MiscellaneousBeans
     @Bean
     CommandLineRunner initDatabase(CourseRepository courseRepository) {
         return args -> {
-            Course myCourse = new Course();
-            myCourse.setTitle("SESC");
-            myCourse.setDescription("Software Engineering for Service Computing");
-            myCourse.setFee(10.00);
-            courseRepository.save(myCourse);
+            Course ISR = new Course();
+            ISR.setTitle("ISR");
+            ISR.setDescription("Intelligent Systems and Robotics");
+            ISR.setFee(20.00);
+            courseRepository.save(ISR);
+
+            Course PGMT = new Course();
+            PGMT.setTitle("PGMT");
+            PGMT.setDescription("Project Management");
+            PGMT.setFee(15.00);
+            courseRepository.save(PGMT);
+
+            Course SESC = new Course();
+            SESC.setTitle("SESC");
+            SESC.setDescription("Software Engineering for Service Computing");
+            SESC.setFee(25.00);
+            courseRepository.save(SESC);
+
+            Course CCD = new Course();
+            CCD.setTitle("CCD");
+            CCD.setDescription("Cloud Computing Development");
+            CCD.setFee(10.00);
+            courseRepository.save(CCD);
+
+            Course SS = new Course();
+            SS.setTitle("SS");
+            SS.setDescription("Smart Systems");
+            SS.setFee(20.00);
+            courseRepository.save(SS);
+
+
         };
     }
 }
